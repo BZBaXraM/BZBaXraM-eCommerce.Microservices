1e447a57340de4e481178f3e4e6bc04306f9d354
Bahram <baxram1997007@gmail.com>
2025-01-15 19:27:25 +0400

    update...

--- eCommerce.ProductsService/eCommerce.Products.DAL/DependencyInjection.cs
+++ eCommerce.ProductsService/eCommerce.Products.DAL/DependencyInjection.cs
@@ -10,15 +10,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDal(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
-        var connectionString = connectionStringTemplate
-            .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
-            .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"))
-            .Replace("$MYSQL_DATABASE", Environment.GetEnvironmentVariable("MYSQL_DATABASE"))
-            .Replace("$MYSQL_PORT", Environment.GetEnvironmentVariable("MYSQL_PORT"))
-            .Replace("$MYSQL_USER", Environment.GetEnvironmentVariable("MYSQL_USER"));
-
-        services.AddDbContext<ProductContext>(options => { options.UseMySQL(connectionString); });
+        services.AddDbContext<ProductContext>(options => { options.UseMySQL(configuration.GetConnectionString("Database")!); });
         services.AddScoped<IProductsRepository, ProductRepository>();
 
         return services;
